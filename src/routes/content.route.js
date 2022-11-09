86548d2531fc05674d771c6d21015cd587ad59a0
berguempaula <berguempaula@gmail.com>
2022-11-09 19:52:10 -0300

    🔄 refactor: refactoring and fixing bugs in "content"

--- src/routes/content.route.js
+++ src/routes/content.route.js
@@ -7,7 +7,7 @@ const route = router.Router();
 route.post('/create', contentController.createContentController);
 route.get('/', contentController.findAllContentController);
 route.get('/findBySubject/:subject', contentController.findContentBySubjectController);
-
-route.patch('/update/:id', contentController.updateContentController);
+route.patch('/:id', contentController.updateContentController);
+route.delete('/:id', contentController.deleteContentController);
 
 export default route;
--- src/services/content.service.js
+++ src/services/content.service.js
@@ -4,11 +4,13 @@ import Content from '../models/Content.js';
 const createContentService = (body) => Content.create(body);
 const findAllContentService = () => Content.find();
 const findContentBySubjectService = (subject) => Content.find({ subject });
-const updateContentService = (content) => Content.updateOne(content);
+const updateContentService = (id, content) => Content.findByIdAndUpdate(id, content);
+const deleteContentService = (id) => Content.findByIdAndDelete(id);
 
 export default {
     createContentService,
     findAllContentService,
     findContentBySubjectService,
     updateContentService,
+    deleteContentService,
 };
