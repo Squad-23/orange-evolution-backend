6c5c75c6084ee93305847cb1ea3cf86fb6eb6f68
Iriedson Souto <iriedson.souto@academico.ifpb.edu.br>
2022-11-13 02:37:34 -0300

    ✨ feat(security): added password encryption

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDatabase from './src/database/db.js';
+import cors from 'cors';
 import userRoute from './src/routes/user.route.js';
 import trailRoute from './src/routes/trail.route.js';
 import moduleRoute from './src/routes/module.route.js';
@@ -12,6 +13,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 connectDatabase();
+app.use(cors());
 app.use(express.json());
 app.use('/user', userRoute);
 app.use('/trail', trailRoute);
--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const UserSchema = new Schema({
     name: {
@@ -30,6 +31,11 @@ const UserSchema = new Schema({
         ref: "Content"
     }]
 
+});
+
+UserSchema.pre('save', async function (next) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
 const User = model('User', UserSchema);
