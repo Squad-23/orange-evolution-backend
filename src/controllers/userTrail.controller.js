db38909ec08a7dd2de4f51bc18c09ee7ac2d4272
Rian Aquino <rian.aquino@outlook.com>
2022-11-15 17:09:28 -0300

    🔄 refactor(usertrail.controller.js): return subdocuments ids in getsubscribedtrails

--- src/controllers/userTrail.controller.js
+++ src/controllers/userTrail.controller.js
@@ -1,6 +1,6 @@
-import userService from '../services/user.service.js';
-import trailService from '../services/trail.service.js';
-import moduleService from '../services/module.service.js';
+import userService from "../services/user.service.js";
+import trailService from "../services/trail.service.js";
+import moduleService from "../services/module.service.js";
 
 const subscribeUserInTrail = async (req, res) => {
     try {
@@ -8,27 +8,27 @@ const subscribeUserInTrail = async (req, res) => {
         const { idTrail } = req.body;
 
         if (!idTrail) {
-            return res.status(400).json({ message: 'Trail Id is missing' });
+            return res.status(400).json({ message: "Trail Id is missing" });
         }
 
         const user = await userService.findByIdUserService(id);
 
         if (!user) {
-            return res.status(404).json({ message: 'User not found' });
+            return res.status(404).json({ message: "User not found" });
         }
 
         const trail = await trailService.findByIdTrailService(idTrail);
 
         if (!trail) {
-            return res.status(404).json({ message: 'Trail not found' });
+            return res.status(404).json({ message: "Trail not found" });
         }
 
         user.trails.push(idTrail);
         const response = await userService.updateUserService(user.id, user);
 
         return res.status(200).json({
-            message: 'User subscribed successfully',
-            trail
+            message: "User subscribed successfully",
+            trail,
         });
     } catch (err) {
         return res.status(500).json({ message: err.message });
@@ -41,75 +41,102 @@ const getUserSubscribedTrails = async (req, res) => {
 
         const user = await userService.getUserSubscribedTrails(id);
 
-        if(!user) {
+        if (!user) {
             res.status(404).json({
-                message: "User not found"
-            })
+                message: "User not found",
+            });
         }
 
-        const trails = await Promise.all(user.trails.map(async (trail) => {
-            if(trail) {
-                let modules = await moduleService.findByTrailIdModuleService(trail._id);
+        const trails = await Promise.all(
+            user.trails.map(async (trail) => {
+                if (trail) {
+                    let modules =
+                        await moduleService.findByTrailIdModuleService(
+                            trail._id
+                        );
 
-                modules = modules.map(module => {
-                    let subjects;
+                    modules = modules.map((module) => {
+                        let subjects;
 
-                    // Getting distincts subjects
-                    subjects = Array.from(new Set(module.contents.map(content => content.subject)));
+                        // Getting distincts subjects
+                        subjects = Array.from(
+                            new Set(
+                                module.contents.map(
+                                    (content) => content.subject
+                                )
+                            )
+                        );
 
-                    // Returning subjects as Objects
-                    subjects = subjects.map(subject => {
-                        if (subject) {
-                            const contents = module.contents
-                                .filter(content => content.subject === subject)
-                                .map(content => ({
-                                    subject: content.subject,
-                                    title: content.title,
-                                    fileType: content.fileType,
-                                    link: content.link,
-                                    time: content.time,
-                                    completed: user.completeds.includes(content._id),
-                                }));
+                        // Returning subjects as Objects
+                        subjects = subjects.map((subject) => {
+                            if (subject) {
+                                const contents = module.contents
+                                    .filter(
+                                        (content) => content.subject === subject
+                                    )
+                                    .map((content) => ({
+                                        id: content._id,
+                                        subject: content.subject,
+                                        title: content.title,
+                                        fileType: content.fileType,
+                                        link: content.link,
+                                        time: content.time,
+                                        completed: user.completeds.includes(
+                                            content._id
+                                        ),
+                                    }));
 
-                            const completeds = contents.filter(content => content.completed);
+                                const completeds = contents.filter(
+                                    (content) => content.completed
+                                );
 
-                            return {
-                                title: subject,
-                                progress: {
-                                    max: contents.length,
-                                    completed: completeds.length
-                                },
-                                contents,
+                                return {
+                                    title: subject,
+                                    progress: {
+                                        max: contents.length,
+                                        completed: completeds.length,
+                                    },
+                                    contents,
+                                };
                             }
-                        }
-                    })
+                        });
 
-                    return ({
-                        title: module.title,
-                        description: module.description,
-                        imageURL: module.imageURL,
+                        return {
+                            id: module._id,
+                            title: module.title,
+                            description: module.description,
+                            imageURL: module.imageURL,
+                            progress: {
+                                max: subjects.length,
+                                completed: subjects.filter(
+                                    ({ progress }) =>
+                                        progress.max &&
+                                        progress.completed === progress.max
+                                ).length,
+                            },
+                            subjects,
+                        };
+                    });
+
+                    return {
+                        id: trail._id,
+                        title: trail.title,
+                        area: trail.area,
+                        description: trail.description,
+                        duration: trail.duration,
                         progress: {
-                            max: subjects.length,
-                            completed: subjects.filter(({ progress }) => progress.max && progress.completed === progress.max).length
+                            max: modules.length,
+                            completed: modules.filter(
+                                ({ progress }) =>
+                                    progress.max &&
+                                    progress.completed === progress.max
+                            ).length,
                         },
-                        subjects,
-                    })
-                })
-
-            return ({
-                _id:  trail._id,
-                title: trail.title,
-                area:  trail.area,
-                description:  trail.description,
-                duration:  trail.duration,
-                progress: {
-                    max: modules.length,
-                    completed: modules.filter(({ progress }) => progress.max && progress.completed === progress.max).length
-                },
-                modules
+                        modules,
+                    };
+                }
             })
-            }
-        }))
+        );
 
         res.json({
             name: user.name,
@@ -119,7 +146,6 @@ const getUserSubscribedTrails = async (req, res) => {
             thisADM: user.thisADM,
             trails,
         });
-
     } catch (err) {
         return res.status(500).send({ message: err.message });
     }
